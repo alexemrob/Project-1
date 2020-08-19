89d069665aa72ff186ff109ab278414acf09e96e
Alexandria Robertson <alexemrob@gmail.com>
2020-08-18 20:47:00 -0500

    change api & js for movies

--- assets/script.js
+++ assets/script.js
@@ -6,53 +6,125 @@ function displayMovieInfo() {
 
   var movie = $("#inputName").val();
   console.log(movie)
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=79d289a4";
+  var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=4e2b560fdefd991469f809e56246ffa2&query=" + movie;
 
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-      console.log(response)
+  }).then(function (response) {
+    console.log(response)
 
     // Creating a div to hold the movie
     var movieDiv = $("<div class='movie'>");
 
-    // Storing the rating data
-    var rating = response.Rated;
+    // Storing the title
+    var movie1title = response.results[0].original_title;
 
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
+    // Creating an element to have the title displayed
+    var pOne = $("<p>").text(movie1title);
 
-    // Displaying the rating
+    // Displaying the title
     movieDiv.append(pOne);
 
     // Storing the release year
-    var released = response.Released;
+    var movie1released = response.results[0].release_date;
 
     // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
+    var pTwo = $("<p>").text("Released: " + movie1released);
 
     // Displaying the release year
     movieDiv.append(pTwo);
 
     // Storing the plot
-    var plot = response.Plot;
+    var movie1plot = response.results[0].overview;
 
     // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
+    var pThree = $("<p>").text("Plot: " + movie1plot);
 
     // Appending the plot
     movieDiv.append(pThree);
 
     // Retrieving the URL for the image
-    var imgURL = response.Poster;
+    var movie1imgURL = response.results[0].poster_path;
 
     // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
+    var image1 = $("<img>").attr("src", "https://image.tmdb.org/t/p/w500" + movie1imgURL);
 
     // Appending the image
-    movieDiv.append(image);
+    movieDiv.append(image1);
+
+    // Storing the title
+    var movie2title = response.results[1].original_title;
+
+    // Creating an element to have the title displayed
+    var pOne2 = $("<p>").text(movie2title);
+
+    // Displaying the title
+    movieDiv.append(pOne2);
+
+    // Storing the release year
+    var movie2released = response.results[1].release_date;
+
+    // Creating an element to hold the release year
+    var pTwo2 = $("<p>").text("Released: " + movie2released);
+
+    // Displaying the release year
+    movieDiv.append(pTwo2);
+
+    // Storing the plot
+    var movie2plot = response.results[1].overview;
+
+    // Creating an element to hold the plot
+    var pThree2 = $("<p>").text("Plot: " + movie2plot);
+
+    // Appending the plot
+    movieDiv.append(pThree2);
+
+    // Retrieving the URL for the image
+    var movie2imgURL = response.results[1].poster_path;
+
+    // Creating an element to hold the image
+    var image2 = $("<img>").attr("src", "https://image.tmdb.org/t/p/w500" + movie2imgURL);
+
+    // Appending the image
+    movieDiv.append(image2);
+
+    // Storing the title
+    var movie3title = response.results[2].original_title;
+
+    // Creating an element to have the title displayed
+    var pOne3 = $("<p>").text(movie3title);
+
+    // Displaying the title
+    movieDiv.append(pOne3);
+
+    // Storing the release year
+    var movie3released = response.results[2].release_date;
+
+    // Creating an element to hold the release year
+    var pTwo3 = $("<p>").text("Released: " + movie3released);
+
+    // Displaying the release year
+    movieDiv.append(pTwo3);
+
+    // Storing the plot
+    var movie3plot = response.results[2].overview;
+
+    // Creating an element to hold the plot
+    var pThree3 = $("<p>").text("Plot: " + movie3plot);
+
+    // Appending the plot
+    movieDiv.append(pThree3);
+
+    // Retrieving the URL for the image
+    var movie3imgURL = response.results[2].poster_path;
+
+    // Creating an element to hold the image
+    var image3 = $("<img>").attr("src", "https://image.tmdb.org/t/p/w500" + movie3imgURL);
+
+    // Appending the image
+    movieDiv.append(image3);
 
     // Putting the entire movie above the previous movies
     $("#results").prepend(movieDiv);
@@ -85,7 +157,7 @@ function renderButtons() {
 }
 
 // This function handles events where a movie button is clicked
-$("#add-movie").on("click", function(event) {
+$("#add-movie").on("click", function (event) {
   event.preventDefault();
   // This line grabs the input from the textbox
   var movie = $("#inputName").val().trim();
